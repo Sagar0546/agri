efb830ce7acaf86884acae3da669d4bca2f55cb6
sagar <150423510+Sagar0546@users.noreply.github.com>
2024-03-04 15:09:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ def predict():
 
 
 
-if __name__ =="__main__":
-	app.debug=True
-	app.run(host = '127.0.0.1',port= 7000)
+if __name__=="__main__":
+    app.run(host='0.0.0.0',port=3000,debug=True)
+
+
